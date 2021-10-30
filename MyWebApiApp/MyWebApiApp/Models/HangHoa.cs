d3a418613980eae073844b27cb14455632e94b71
Hien Luong <hyhien@gmail.com>
2021-10-30 09:16:54 +0700

    Intro WebAPI

---
+++ MyWebApiApp/MyWebApiApp/Models/HangHoa.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyWebApiApp.Models
+{
+    public class HangHoaVM
+    {
+        public string TenHangHoa { get; set; }
+        public double DonGia { get; set; }
+    }
+
+    public class HangHoa : HangHoaVM
+    {
+        public Guid MaHangHoa { get; set; }
+    }
+}
